7ee9557a605da4aa13b1efb130f9d3d84c573d10
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2022-01-11 21:35:36 +0000

    calculate dna of new zombie

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -4,11 +4,14 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
-  function feedAndMultiply (uint _zombieId, uint _targetDna) public{
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    
+    _targetDna = _targetDna % dnaModulus;
 
-      require(msg.sender == zombieToOwner[_zombieId]);
-      Zombie storage myZombie = zombies[_zombieId];
-
+    uint newDna = (myZombie.dna + _targetDna)/2;
+    _createZombie("NoName", newDna);
 
   }
 
