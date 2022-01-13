ae08a3c8d981620e37eff12dbfb1e11630f93f1f
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2022-01-13 22:27:24 +0000

    create interface and initialize kittycontract

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -2,9 +2,8 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombiefactory.sol";
 
-// Create kitty interface
 contract KittyInterface {
-function getKitty(uint256 _id) external view returns (
+  function getKitty(uint256 _id) external view returns (
     bool isGestating,
     bool isReady,
     uint256 cooldownIndex,
@@ -18,9 +17,15 @@ function getKitty(uint256 _id) external view returns (
   );
 }
 
-
-
 contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+
+
+
+  // Initialize kittyContract using `ckAddress`
+
+  KittyInterface kittyContract = KittyInterface(ckAddress);
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
