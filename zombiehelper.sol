7ff1da2558c64274a91470155d7c0a5d6fd51407
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2022-01-25 22:26:56 +0000

    Using memory keyword and array to save on storage.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -19,9 +19,13 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].dna = _newDna;
   }
 
-  // Create your function here
+  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
 
-  function getZombiesByOwner(address _owner) external view returns (uint[] memory) {
+    return result;
+
+
+
 
 
 
